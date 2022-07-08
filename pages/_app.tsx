6f68a48d846c014623508d3f993b2d30aa14af3d
VladKrauchuk <krauchukvlad@gmail.com>
2022-07-08 13:52:14 +0300

    Styles: variables

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/variables.css'
+import type {AppProps} from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+    return <Component {...pageProps} />
 }
 
 export default MyApp
